85f0a5d4bd1dd46ef461beabd81ee1843164bb85
Jisu Han <weblite@naver.com>
2022-03-06 15:33:28 +0900

    log 찍기 복습 @Slf4J,@RestController(View이름 반환x)

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // @Controller 를 사용하면 뷰 이름을 반환하기 때문에 @RestController 애노테이션 사용
 @Slf4j
 public class LogTestController {
 //    private final Logger log= LoggerFactory.getLogger(getClass());
@@ -17,10 +17,8 @@ public class LogTestController {
     @RequestMapping("/log-test")
     public String logTest(){
         String name ="Spring";
-
         log.trace("trace log={}",name);
         log.debug("debug log={}",name);
-
         /* info 레벨만 출력되도록 하는 것을 application.properties에서 설정가능 -> soutv 로 설정하면
          로그 정보가 다 남게 된다. 필요한 로그 정보만 남기도록 하기 위함이다.
          개발서버에서는 logging.level.hello.springmvc=debug ,
@@ -33,3 +31,6 @@ public class LogTestController {
         return "ok";
     }
 }
+// 밑과 같은 방식으로 로그가 남게 된다.
+//2022-03-06 13:34:09.182  WARN 8236 --- [nio-8080-exec-1] hello.springmvc.basic.LogTestController  : warn log=Spring
+//2022-03-06 13:34:09.183 ERROR 8236 --- [nio-8080-exec-1] hello.springmvc.basic.LogTestController  : error log=Spring
